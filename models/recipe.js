342048f005e9cfa2b603f384c88854b8f8d142dc
James Albert Phillips V <jamesalbertphillipsv@airbears2-10-142-152-31.airbears2.1918.berkeley.edu>
2018-09-14 14:28:32 -0700

    final friday push

--- models/recipe.js
+++ models/recipe.js
@@ -1,5 +1,4 @@
-module.exports = function(sequelize, //DataTypes
-  ) {
+module.exports = function(sequelize, DataTypes) {
   var Recipe = sequelize.define("Recipe", {
     body :{
       type : DataTypes.TEXT,
