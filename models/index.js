c1953c1f7f279ec7a01c83596dd90fa1d7e93399
James Albert Phillips V <jamesalbertphillipsv@airbears2-10-142-153-57.airbears2.1918.berkeley.edu>
2018-09-18 11:50:31 -0700

    signup authentication

--- models/index.js
+++ models/index.js
@@ -3,16 +3,14 @@
 var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
-// var sequelize = require("../config/connection.js");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require('../config/config.json')[env];
+var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} 
-else {
+} else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
@@ -24,7 +22,6 @@ fs
   .forEach(function(file) {
     var model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
-    console.log(model)
   });
 
 Object.keys(db).forEach(function(modelName) {
