80610b1115d7d9474e319c1d155bc50666a1de3c
Andrew Norris <andrewmnorris@gmail.com>
2018-09-19 10:10:30 -0700

    added back in name in routes

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -115,6 +115,7 @@ module.exports = function(app) {
       // Otherwise send back the user's email and id
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
+        name: req.user.name,
         email: req.user.email,
         id: req.user.id,
         preference: req.user.userPreference,
